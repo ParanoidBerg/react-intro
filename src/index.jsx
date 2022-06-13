f99f6d008af3315be8912575caf3bcf55b1c1f77
Paranoid <magomaevd99@gmail.com>
2022-06-13 11:57:32 +0300

    task is completed

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <>
+    <ul className="based">BASED af
+       <li>not cringe at all</li>
+    </ul>
+    </>
+)
